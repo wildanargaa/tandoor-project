6ca5bfa0712ea9dba44a5fe48430b10238305f86
wildanargaa <161486070+wildanargaa@users.noreply.github.com>
2024-05-19 23:44:01 +0700

    First Commit

--- lib/main.dart
+++ lib/main.dart
@@ -32,3 +32,12 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
